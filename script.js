358fcfcdb9c2b22ee59e90b5cc06a7439b9ac0e1
Smith Thongbai <116685524+thonsm06@users.noreply.github.com>
2025-03-28 00:45:39 -0500

    Add function to display books Add basic css to see books

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 const library = [];
 
-function CreateBook(title, author, id) {
+ 
+
+function CreateBook(title, author, pages, id) {
     this.title = title;
     this.author = author;
+    this.pages = pages;
     this.id = id;
 }
 
-function addBookToLibrary(title, author) {
-    let id = crypto.randomUUID();
-    library.push(new CreateBook(title, author, id));
+function addBookToLibrary(title, author, pages) {
+    const id = crypto.randomUUID();
+    library.push(new CreateBook(title, author, pages, id));
 
 }
 
+addBookToLibrary("LotR", "Tokien", "1000");
+
+function displayBooks(library) {
+    for (book of library) {
+        const div = document.createElement("div");
+        // div.classList.add("book container");
+        const title = document.createElement("h2");
+        title.textContent = book.title;
+        const author = document.createElement("p");
+        author.textContent = book.author;
+        const pages = document.createElement("p");
+        pages.textContent = book.pages;
+        div.appendChild(title)
+        div.appendChild(author);
+        div.appendChild(pages);
+        document.body.appendChild(div);
+        
+    }
+}
+
+displayBooks(library);
