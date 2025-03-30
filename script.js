822cae51e2634e5e906e7cbdbec3ed1aa0438d2c
Smith Thongbai <116685524+thonsm06@users.noreply.github.com>
2025-03-30 00:43:42 -0500

    Fixing findIndex function

--- script.js
+++ script.js
@@ -1,20 +1,55 @@
 const library = [];
 
-function CreateBook(title, author, pages, id) {
+function CreateBook(title, author, pages, id, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.id = id;
+    this.read = read;
+}
+CreateBook.prototype.toggleRead = function() {
+    if (this.read === false) {
+        this.read = true;
+    } else {
+        this.read = false;
+    }
+};
+
+function addBookToLibrary(title, author, pages, id, read) {
+    library.push(new CreateBook(title, author, pages, id, read));
 }
 
-function addBookToLibrary(title, author, pages) {
-    const id = crypto.randomUUID();
-    library.push(new CreateBook(title, author, pages, id));
-
+//toggle status of array and display
+function toggleStatus(bookDiv) {
+    const status = bookDiv.getAttribute("value");
+    if (status === false) {
+        bookDiv.setAttribute("value", true);
+        
+    } else {
+        bookDiv.setAttribute("value", false);
+        
+    }
 }
-
+function setReadStatus(bookDiv) {
+    const status = bookDiv.getAttribute("value");   
+    if (status === false) {
+        book.textContent = "unread";
+    } else {
+        book.textContent = "read";
+    }
+}
+function removeBookFromArray(event) {
+    const index = library.findIndex((book) => {
+        return book.id === event.target.getAttribute("data-id");
+    });
+    if (index !== -1) {
+       library.splice(index, 1);
+        
+    };
+}
 function displayBooks(library) {
     let i = 1; //track book being display
+    const contents = document.querySelector(".content-container");
     for (book of library) {
         let bookCard = document.querySelector(`.book-card-${i}`);
         if (bookCard === null) { //if book has not been displayed
@@ -26,10 +61,54 @@ function displayBooks(library) {
             author.textContent = book.author;
             const pages = document.createElement("p");
             pages.textContent = book.pages;
+            const remove = document.createElement("button");
+            remove.textContent = "Remove";
+            remove.setAttribute("data-id", book.id);
+            remove.addEventListener("click", removeBookFromArray) ;
+            console.log(library);
+                // const bookIndexInLibrary = library.findIndex((this) => {
+                //     return this.id === e.target.getAttribute("data-id");
+                // })
+            
+            // remove.addEventListener("click", (event) => {
+            //     const bookIndexInLibrary = library.findIndex((b) => {
+            //         return b.id === event.target.getAttribute("data-id");
+            //     });
+            //     if (bookIndexInLibrary !== -1) {
+            //         //console.log(`index: ${bookIndexInLibrary}`);
+            //         library.splice(bookIndexInLibrary, 1);
+            //         bookCard.remove();
+            //         //console.log(library);
+            //     }
+            // })
+
+            const read = document.createElement("div");
+            read.classList.toggle(`read-${i}`);
+            read.setAttribute("value", book.read);
+            read.setAttribute("date-id", book.id);
+            read.addEventListener("click", (event) => {
+                //get array element
+                //console.log(event.target.getAttribute("data-id"));
+                const bookIndexInLibrary = library.indexOf(event.target.getAttribute("data-id"));
+                library[bookIndexInLibrary].toggleRead();
+
+                toggleStatus(event.target);
+            })
+            if (book.read === false) {
+                read.style.backgroundColor = "gray";
+                read.textContent = "unread";
+            } else {
+                read.style.backgroundColor = "green";
+                read.textContent = "read";
+            }
+           
             bookCard.appendChild(title)
             bookCard.appendChild(author);
             bookCard.appendChild(pages);
-            document.body.appendChild(bookCard);
+            bookCard.appendChild(read);
+            bookCard.appendChild(remove);
+            
+            contents.appendChild(bookCard);
         }
         i++
     }
@@ -43,7 +122,14 @@ add.addEventListener("click", (event) => {
     const title = document.querySelector("#title").value;
     const author = document.querySelector("#author").value;
     const pages = document.querySelector("#pages").value;
-    addBookToLibrary(title, author, pages);
+    const id = crypto.randomUUID();
+    const read = document.querySelector("#read").checked;
+    
+    addBookToLibrary(title, author, pages, id, read);
     displayBooks(library);
     event.preventDefault();
+
+    //console.log(library);
 });
+
+
