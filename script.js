85d5d5b9709a32b24aa48888af12f9f2c2df87e4
Smith Thongbai <116685524+thonsm06@users.noreply.github.com>
2025-03-30 04:07:05 -0500

    Finally fixed read/unread status update

--- script.js
+++ script.js
@@ -8,10 +8,12 @@ function CreateBook(title, author, pages, id, read) {
     this.read = read;
 }
 CreateBook.prototype.toggleRead = function() {
-    if (this.read === false) {
+    if (this.read == false) {
         this.read = true;
+        return true;
     } else {
         this.read = false;
+        return false;
     }
 };
 
@@ -20,70 +22,99 @@ function addBookToLibrary(title, author, pages, id, read) {
 }
 
 //toggle status of array and display
-function toggleStatus(bookDiv) {
-    const status = bookDiv.getAttribute("value");
-    if (status === false) {
-        bookDiv.setAttribute("value", true);
+// function toggleStatus(bookDiv) {
+//     const status = bookDiv.getAttribute("value");
+//     if (status === false) {
+//         bookDiv.setAttribute("value", true);
         
-    } else {
-        bookDiv.setAttribute("value", false);
+//     } else {
+//         bookDiv.setAttribute("value", false);
         
-    }
-}
-function setReadStatus(bookDiv) {
-    const status = bookDiv.getAttribute("value");   
-    if (status === false) {
-        book.textContent = "unread";
-    } else {
-        book.textContent = "read";
-    }
-}
+//     }
+// }
+// function setReadStatus(bookDiv) {
+//     const status = bookDiv.getAttribute("value");   
+//     if (status === false) {
+//         book.textContent = "unread";
+//     } else {
+//         book.textContent = "read";
+//     }
+// }
 
 const container = document.querySelector(".content-container");
-container.addEventListener("click", (event) => {
+container.addEventListener("click", function (event) {
     const target = event.target;
-    switch(target) {
-        //case: 
-    }
+    const className = target.classList.value;
+    
     const bookIndexInLibrary = library.findIndex((book) => {
         return book.id === target.getAttribute("data-id");
     });
-    if (bookIndexInLibrary !== -1) {
-        library.splice(bookIndexInLibrary, 1);
-        target.parentElement.remove();
+
+    switch(className) {
+        case "remove-button":
+            
+            if (bookIndexInLibrary !== -1) {
+                library.splice(bookIndexInLibrary, 1);
+                target.parentElement.remove();
+            }
+            break;
+        case "read":            
+            //const status = target.getAttribute("value");
+            library[bookIndexInLibrary].toggleRead();
+            const status = library[bookIndexInLibrary].read;
+            //console.log(status);
+            console.log(status);
+            //console.log(library[bookIndexInLibrary].read);
+            if (status === false) {
+                //console.log(1);
+                //library[bookIndexInLibrary].read = true;
+                target.textContent = "unread";
+                target.setAttribute("value", false);
+                target.style.backgroundColor = "gray";
+            } else {
+                //console.log(2);
+                //library[bookIndexInLibrary].read = false;
+                target.textContent = "read";
+                target.setAttribute("value", true);
+                target.style.backgroundColor = "green";
+            }
+            //console.log(target.getAttribute("value"));
+            //setReadStatus(target);
+            console.log(target);
+            break;
     }
 });
 
+let numberOfBooksToDisplay = 20; //this can be change by user
 function displayBooks(library) {
     let i = 1; //track book being display
     const contents = document.querySelector(".content-container");
-    for (book of library) {
-        let bookCard = document.querySelector(`.book-card-${i}`);
-        if (bookCard === null) { //if book has not been displayed
+    //limit how many books to display
+    const startingIndex = 0; //set to 1 for id purpose, increment at let i.
+    numberOfBooksToDisplay = library.length < 20 ? library.length : 20;
+    for(let i = startingIndex; i < startingIndex + numberOfBooksToDisplay; i++) {
+        const book = library[i];
+        let bookCard = document.querySelector(`#book-${i}`);
+        if (bookCard === null) {
             bookCard = document.createElement("div");
-            bookCard.classList.toggle(`book-card-${i}`);
-            const title = document.createElement("h2");
+            bookCard.classList.toggle("book-card");
+            bookCard.id = `book-${i}`;
+            const title = document.createElement("h3");
             title.textContent = book.title;
             const author = document.createElement("p");
             author.textContent = book.author;
             const pages = document.createElement("p");
             pages.textContent = book.pages;
             const remove = document.createElement("button");
+            remove.classList.toggle("remove-button");
             remove.textContent = "Remove";
             remove.setAttribute("data-id", book.id);
 
             const read = document.createElement("div");
-            read.classList.toggle(`read-${i}`);
+            read.classList.toggle("read");
             read.setAttribute("value", book.read);
-            read.setAttribute("date-id", book.id);
-            read.addEventListener("click", (event) => {
-                //get array element
-                //console.log(event.target.getAttribute("data-id"));
-                const bookIndexInLibrary = library.indexOf(event.target.getAttribute("data-id"));
-                library[bookIndexInLibrary].toggleRead();
+            read.setAttribute("data-id", book.id);
 
-                toggleStatus(event.target);
-            })
             if (book.read === false) {
                 read.style.backgroundColor = "gray";
                 read.textContent = "unread";
@@ -91,7 +122,7 @@ function displayBooks(library) {
                 read.style.backgroundColor = "green";
                 read.textContent = "read";
             }
-           
+
             bookCard.appendChild(title)
             bookCard.appendChild(author);
             bookCard.appendChild(pages);
@@ -100,9 +131,59 @@ function displayBooks(library) {
             
             contents.appendChild(bookCard);
         }
-        i++
     }
 }
+
+    //for (book of library) {
+        //check if book is already display using book's id
+        
+        // let bookCard = document.querySelector(`.book-card-${i}`);
+        // if (bookCard === null) { //if book has not been displayed
+        //     bookCard = document.createElement("div");
+        //     bookCard.classList.toggle(`book-card-${i}`);
+        //     bookCard.id = book.id;
+        //     const title = document.createElement("h2");
+        //     title.textContent = book.title;
+        //     const author = document.createElement("p");
+        //     author.textContent = book.author;
+        //     const pages = document.createElement("p");
+        //     pages.textContent = book.pages;
+        //     const remove = document.createElement("button");
+        //     remove.classList.toggle("remove-button");
+        //     remove.textContent = "Remove";
+        //     remove.setAttribute("data-id", book.id);
+
+        //     const read = document.createElement("div");
+        //     read.classList.toggle(`read-${i}`);
+        //     read.setAttribute("value", book.read);
+        //     read.setAttribute("date-id", book.id);
+            //read.addEventListener("click", (event) => {
+                //get array element
+                //console.log(event.target.getAttribute("data-id"));
+                //const bookIndexInLibrary = library.indexOf(event.target.getAttribute("data-id"));
+                //library[bookIndexInLibrary].toggleRead();
+
+                //toggleStatus(event.target);
+            //})
+            // if (book.read === false) {
+            //     read.style.backgroundColor = "gray";
+            //     read.textContent = "unread";
+            // } else {
+            //     read.style.backgroundColor = "green";
+            //     read.textContent = "read";
+            // }
+           
+//             bookCard.appendChild(title)
+//             bookCard.appendChild(author);
+//             bookCard.appendChild(pages);
+//             bookCard.appendChild(read);
+//             bookCard.appendChild(remove);
+            
+//             contents.appendChild(bookCard);
+//         }
+//         i++
+//     }
+// }
 
 displayBooks(library);
 
@@ -118,7 +199,7 @@ add.addEventListener("click", (event) => {
     addBookToLibrary(title, author, pages, id, read);
     displayBooks(library);
     event.preventDefault();
-    console.log(library);
+    //console.log(library);
 });
 
 
